b2db6a80152b46b8a673d3ea466561573403b5e4
Mankevich Nadya <nadya.mankevichhh@mail.ru>
2020-04-08 15:38:37 +0300

    w

--- 21dz/ViewController.swift
+++ 21dz/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
-
+import SDWebImage
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var graySwitch: UISwitch!
+    @IBOutlet weak var blurSwitch: UISwitch!
+    @IBOutlet weak var sliderChange: UISlider!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func refreshButton(_ sender: Any) {
+        let url = generateURL()
+        imageView.sd_setImage(with: url, placeholderImage: nil, options: .fromLoaderOnly)
+    }
+    private func generateURL() -> URL {
+        var stringURL = "https://picsum.photos/1920/1080?"
+        if graySwitch.isOn {
+            stringURL.append("grayscale")
+        }
+        
+        if blurSwitch.isOn {
+            let blurValue = Int(sliderChange.value)
+            if graySwitch.isOn {
+                stringURL.append("&")
+            }
+            stringURL.append("blur=\(blurValue)")
+        }
+        let url = URL(string: stringURL)!
+        return url
+    }
 }
 
